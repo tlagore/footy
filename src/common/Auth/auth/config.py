572627aa1e51620df91e4711dc71e63c2ada8c8b
tlagore <tyronelagore@gmail.com>
2019-02-23 23:39:51 -0800

    made Auth an installable package

---
+++ src/common/Auth/auth/config.py
@@ -0,0 +1,9 @@
+KMConfig = {
+    "TENANT_ID" : "601ec5fe-3c4e-4ba2-b15c-3ff7ad272a7e",
+    "CLIENT_ID" : "2795bb93-a063-4c9e-a7cb-ec9a66f0efc6",
+    "THUMBPRINT" : "233E44576FED0A3993DB9253F6F8B5043184C69D",
+    "KV_URI" : "https://footkv.vault.azure.net",
+    # this file must be downloaded from the keyvault manually for local development
+    # DO NOT COMMIT THIS FILE TO VERSION CONTROL. If you name it <name>.pem .gitignore will ignore it
+    "PKEY_FILE" : "footy-pk.pem"
+}
--- src/common/Auth/setup.py
+++ src/common/Auth/setup.py
@@ -4,5 +4,7 @@ setup(
     name='Auth',
     version='0.1dev',
     packages=['auth',],
-    license=''
+    install_requires=['adal==1.2.1'],
+    license='',
+    long_description=open('README').read()
 )
