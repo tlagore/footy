5e0134f82ca105077ac8e24b9483f0745bb0b8a1
tlagore <tyronelagore@gmail.com>
2019-02-24 20:53:59 -0800

    secret typo

--- src/scripts/set-app-secret-env.ps1
+++ src/scripts/set-app-secret-env.ps1
@@ -1,4 +1,4 @@
-﻿$sec = az keyvault secret show --vault-name footkv --name FootyAppSecet
+﻿$sec = az keyvault secret show --vault-name footkv --name FootyAppSecret
 $secretvalue = ($sec | ConvertFrom-Json).value
 
 [System.Environment]::SetEnvironmentVariable("FOOTY_APP_SECRET", $secretvalue, "Machine")
